5e76d6d8052058ed54d048f09463a15055445306
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-05-24 16:36:34 +0800

    Add response to help command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (std::strcmp(argv[1], "-h") == 0) {
+        std::cout
+                << "Please input two file arguments. First file must be in FASTA format. Second file can be in either FASTA or FASTQ format"
+                << std::endl;
+        return 0;
+    }
+
     return 0;
 }
